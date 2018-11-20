a85baf180ad6ee0295e42cf01e8b310338326748
Ashik Antala <Ashik.Antala@b2x.com>
2018-11-20 10:26:43 +0530

    Create queries.js
    Add operations service

---
+++ services/queries.js
@@ -0,0 +1,90 @@
+const q = require('q')
+const dbQuery = require('./dbQuery')
+const dbName = 'INDX'
+
+function createOperation (queryString) {
+  let deferred = q.defer()
+
+  dbQuery.select(queryString, dbName)
+    .then((response) => {
+      deferred.resolve({
+        status: 'success',
+        message: '',
+        data: response
+      })
+    })
+    .catch((error) => {
+      deferred.reject({
+        status: 'failed',
+        error: error
+      })
+    })
+
+  return deferred.promise
+}
+
+function readOperation (queryString) {
+  let deferred = q.defer()
+  dbQuery.select(queryString, dbName)
+    .then((response) => {
+      deferred.resolve({
+        status: 'success',
+        message: '',
+        data: response
+      })
+    })
+    .catch((error) => {
+      deferred.reject({
+        status: 'failed',
+        error: error
+      })
+    })
+
+  return deferred.promise
+}
+
+function updateOperation (queryString) {
+  let deferred = q.defer()
+
+  dbQuery.select(queryString, dbName)
+    .then((response) => {
+      deferred.resolve({
+        status: 'success',
+        message: '',
+        data: response
+      })
+    })
+    .catch((error) => {
+      deferred.reject({
+        status: 'failed',
+        error: error
+      })
+    })
+
+  return deferred.promise
+}
+
+function deleteOperation (queryString) {
+  let deferred = q.defer()
+
+  dbQuery.select(queryString, dbName)
+    .then((response) => {
+      deferred.resolve({
+        status: 'success',
+        message: 'User Delete Successfully.',
+        data: response
+      })
+    })
+    .catch((error) => {
+      deferred.reject({
+        status: 'failed',
+        error: error
+      })
+    })
+
+  return deferred.promise
+}
+
+module.exports = {
+  createOperation, readOperation, updateOperation, deleteOperation
+}
